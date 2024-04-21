381fbe6b3f775a32784d97106571e48f1aa42d9c
Alikhan9292 <adaetskz@gmail.com>
2024-04-21 19:56:00 +0600

    image and user

--- src/main/java/com/example/buysell/controllers/ProductController.java
+++ src/main/java/com/example/buysell/controllers/ProductController.java
@@ -5,10 +5,10 @@ import com.example.buysell.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,14 +23,25 @@ public class ProductController {
 
     @GetMapping("/product/{id}")
     public String productInfo(@PathVariable Long id, Model model) {
-        model.addAttribute("product", productService.getProductById(id));
+        Product product = productService.getProductById(id);
+        model.addAttribute("product", product);
+        model.addAttribute("images", product.getImages());
         return "product-info";
     }
 
     @PostMapping("/product/create")
-    public String createProduct(Product product) {
-        productService.saveProduct(product);
+    public String createProduct(@RequestParam("file1") MultipartFile file1, @RequestParam("file2") MultipartFile file2,
+                                @RequestParam("file3") MultipartFile file3, Product product) throws IOException {
+        productService.saveProduct(product, file1, file2, file3);
         return "redirect:/";
+    }
+
+    @PostMapping("/product/update/{id}")
+    public String updateProduct(@PathVariable Long id, @RequestParam("file1") MultipartFile file1,
+                                @RequestParam("file2") MultipartFile file2, @RequestParam("file3") MultipartFile file3,
+                                Product updatedProduct) throws IOException {
+        productService.updateProduct(id, updatedProduct, file1, file2, file3);
+        return "redirect:/product/{id}";
     }
 
     @PostMapping("/product/delete/{id}")
